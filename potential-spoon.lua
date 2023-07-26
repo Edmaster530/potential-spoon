278fd87e499ac030c78280dce2d05799711986ba
Edward Bala <edward.bala@hotmail.com>
2023-07-25 21:22:29 -0500

    It works!

--- potential-spoon.lua
+++ potential-spoon.lua
@@ -1,27 +1,7 @@
-
-local folder,data=...;  -- Addon Folder Name, AddonTable Data Object
-PotentialSpoonAddon = LibStub("AceAddon-3.0"):NewAddon(folder);
+local _,data=...;  -- Addon Folder Name, AddonTable Data Object
+PotentialSpoonAddon = LibStub("AceAddon-3.0"):NewAddon(data.Directory);
 local AceConfigRegistry = LibStub("AceConfigRegistry-3.0");
 local AceConfigDialog = LibStub("AceConfigDialog-3.0");
-
-local base_options = {
-    defaults = {
-        profile = {},
-        global = {
-            damage_font = 1,
-        },
-    },
-    options.fonts = {
-        1 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\pepsi.ttf",
-        2 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\basket-of-hammers.ttf",
-        3 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\college.ttf",
-        4 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\elite.ttf",
-        5 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\galaxy-one.ttf",
-        6 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\skratch-punk.ttf",
-        7 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\stentiga.ttf",
-        8 = "Interface\\Addons\\#69 Suffering - Refreshed\\Fonts\\zombie.ttf",
-    },
-};
 
 --[[
     UI Types are "cmd" "dropdown" "dialog" (ignored)
@@ -32,31 +12,34 @@ local base_options = {
     App Name options are stuck to one type, we don't have multiples
 ]]
 function PotentialSpoonAddon:GetOptions(_, _, name)
-    local options = {
+    return {
         type = "group",
-        name = self.MetaData.Name .. " (" .. self.MetaData.Version .. ")",
+        name = data.MetaData.Name .. " (" .. data.MetaData.Version .. ")",
         args = {
             damage_font = {
                 type = "select",
                 style = "dropdown",
                 name = "Font",
-                values = base_options.fonts,
+                values = data.FontLookup,
                 set = function(_, value)
-                    self.db.global.damage_font = value;
-                    DAMAGE_TEXT_FONT = value;
+                    PotentialSpoonAddon.db.global.damage_font = value;
+                    DAMAGE_TEXT_FONT = data.FontDictionary[value];
+                end,
+                get = function(_)
+                    return PotentialSpoonAddon.db.global.damage_font;
                 end
             },
         },
-    }
+    };
 end
 
 --[[
     AceAddon Initialization Logic
 ]]
 function PotentialSpoonAddon:OnInitialize()
-    self.db = LibStub("AceDB-3.0"):New(data.DatabaseName, base_options.defaults, true);
-    AceConfigRegistry:RegisterOptionsTable(folder, PotentialSpoonAddon.GetOptions);
-    data.Dialog = AceConfigDialog:AddToBlizOptions(folder, data.MetaData.Name);
+    self.db = LibStub("AceDB-3.0"):New(data.DatabaseName, data.DefaultOptions, true);
+    AceConfigRegistry:RegisterOptionsTable(data.MetaData.Name, PotentialSpoonAddon.GetOptions);
+    data.Dialog = AceConfigDialog:AddToBlizOptions(data.MetaData.Name, data.MetaData.Name);
     -- setting the damage text font from the options configured
-    DAMAGE_TEXT_FONT = PotentialSpoonAddon:SetFont(self.db.global.damage_font);
+    DAMAGE_TEXT_FONT = data.FontDictionary[self.db.global.damage_font];
 end
--- variables.lua
+++ variables.lua
@@ -1,10 +1,37 @@
-local folder,data=...;
+local _,data=...;
 
 -- Addon Constants --
+data.Directory = "potential-spoon";
 data.DatabaseName = "PotentialSpoonDB";
 data.MetaData = {
     Name = GetAddOnMetadata(..., "Title"),
     Version = GetAddOnMetadata(..., "Version"),
     Notes = GetAddOnMetadata(..., "Notes"),
 };
+
+data.DefaultOptions = {
+    profile = {},
+    global = {
+        damage_font = 1,
+    },
+};
+
 data.Dialog = {};
+data.FontDictionary = {};
+data.FontDictionary[1] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\pepsi.ttf";
+data.FontDictionary[2] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\basket-of-hammers.ttf";
+data.FontDictionary[3] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\college.ttf";
+data.FontDictionary[4] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\elite.ttf";
+data.FontDictionary[5] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\galaxy-one.ttf";
+data.FontDictionary[6] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\skratch-punk.ttf";
+data.FontDictionary[7] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\stentiga.ttf";
+data.FontDictionary[8] = "Interface\\Addons\\" .. data.Directory .. "\\Fonts\\zombie.ttf";
+data.FontLookup = {};
+data.FontLookup[1] = "! PEPSI !";
+data.FontLookup[2] = "Basket of Hammers";
+data.FontLookup[3] = "College";
+data.FontLookup[4] = "Elite";
+data.FontLookup[5] = "Galaxy 1";
+data.FontLookup[6] = "Skratch Punk";
+data.FontLookup[7] = "Stentiga";
+data.FontLookup[8] = "Zombie";
